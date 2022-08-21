136e5e0234a76cee9d6c8c57807d11adbd029f29
shirokovnv <shirokovnv@gmail.com>
2022-08-21 13:49:41 +0500

    Format docker entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,10 +11,11 @@ done
 # Create, migrate, and seed database if it doesn't exist.
 if [[ -z `psql -Atqc "\\list $PGDATABASE"` ]]; then
   echo "Database $PGDATABASE does not exist. Creating..."
-  #createdb -E UTF8 $PGDATABASE -l en_US.UTF-8 -T template0
+
   mix ecto.setup
   mix ecto.migrate
   mix run priv/repo/seeds.exs
+
   echo "Database $PGDATABASE created."
 fi
 
